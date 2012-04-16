5fa8339b2a7735fbdbdc4e7adc7601f91b015ad0
Nenzo <roman.frei1@swisscom.com>
2012-04-16 16:36:45 +0200

    Completed changeids method, commented and wrote exception handlers

--- app/controllers/skyrequests_controller.rb
+++ app/controllers/skyrequests_controller.rb
@@ -1,25 +1,33 @@
 class SkyrequestsController < ApplicationController
 
 before_filter :skylogic_login
+before_filter :get_string, :only => :create #avoid wrong inputs (strings)
 
   def index 
 	@skyrequest = Skyrequest::Connector.all.first
   end
 
+  def get_string #find letters in params[:value] string
+	@value = params[:value]
+	if @value.match('\D').present?
+		redirect_to skyrequests_path
+		flash[:error] = "Only Integers allowed."
+	end
+  end
+
   def create	
-	if Skyrequest::Connector.all.present?
+	if Skyrequest::Connector.all.present? #create new class variable or choose an existing one
 		@skyrequest = Skyrequest::Connector.all.first
 	else
 		@skyrequest = Skyrequest::Connector.new
 		@skyrequest.save_cache
 	end
-
-	@skyrequest.setindex
-    @skylogic_data = @agent.call("tooway.account.getDetails", {"sai" => params[:value].to_s})
-	@skylogic_data["value"] = params[:value]
-
-	@skyrequest.save(@skylogic_data)
-	@skyrequest.seterrors
+	@skyrequest.changeids #re-index cache
+	@skyrequest.setindex #calculate index, range 0 to 5
+   	@skylogic_data = @agent.call("tooway.account.getDetails", {"sai" => @value.to_s.gsub(/\s+/, "")}) #skylogic request with method and value
+	@skylogic_data["value"] = @value #write form value into server response
+	@skyrequest.save(@skylogic_data) #save response into cache
+	@skyrequest.seterrors #rename integer errors into strings
 
 	redirect_to skyrequests_path
   end
--- lib/skyrequest.rb
+++ lib/skyrequest.rb
@@ -3,15 +3,15 @@ module Skyrequest
 	class Connector
 	attr_accessor :cache, :index, :counter
 
-	@@skycaches = []
+	@@skycaches = [] #class variable
 
-		def initialize()
+		def initialize() #Sky::Connector.new
 			@cache = {}
 			@index = 0
 			@counter = -1
 		end
 
-		def save_cache
+		def save_cache #save variables after new controller action
 			@@skycaches << self
 		end
 
@@ -19,32 +19,48 @@ module Skyrequest
 			return @cache
 		end
 		
-		def setindex
+		def setindex #calculate index with modulo, range 0 to 5
 			@counter += 1
 			@index = @counter % 6
 		end
 
-		def save(data)
-			@cache[@index] = data
+		def save(data) #write skylogic answer into cache
+			@cache[0] = data
 		end
 	
-		def seterrors
+		def seterrors #rename integer errors into strings
 			unless @cache.empty?
-			case @cache[@index]["status"]
-				when 0
-					@cache[@index]["status"] = "OK"
-				when 1
-					@cache[@index]["status"] = "Warning"
-				when 2
-					@cache[@index]["status"] = "Error"
-				else
-					@cache[@index]["status"] = "Unknown Status"
-			end
+				case @cache[0]["status"]
+					when 0
+						@cache[0]["status"] = "OK"
+					when 1
+						@cache[0]["status"] = "Warning"
+					when 2
+						@cache[0]["status"] = "Error"
+					else
+						@cache[0]["status"] = "Unknown Status"
+				end
 			end
 		end
 
-		def self.all
+		def self.all #debug
 			return @@skycaches
+		end
+
+		def changeids #newest request always on top
+			unless @cache.empty?
+				@temp_cache = {}
+				@max_cache = @cache.count
+				@keys = 0
+				while @keys < @max_cache #write cache into temp_cache
+					@temp_cache[@keys] = @cache[@keys]
+					@keys += 1
+				end
+				while @keys > -1 #write temp_cache back into cache but increase index by 1
+					@cache[@keys] = @temp_cache[@keys-1]
+					@keys -= 1
+				end
+			end
 		end
 	end
 end
